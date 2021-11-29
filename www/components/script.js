b5c554b59f4d66f984314574d47f5181d6a3c69b
erikitas <erikathompson818@gmail.com>
2021-11-30 00:19:10 +0900

    Project's Initial Commit

---
+++ www/components/script.js
@@ -0,0 +1,69 @@
+// This is a JavaScript file
+
+$(document).on('click', '#rodolfo', function(){
+  alert("Resposta Correta!");
+  var correto = "correto";
+});
+$(document).on('click', '#elis', function(){
+  alert("Resposta Errada! Tente Novamente!");
+  var correto = "errado";
+});
+$(document).on('click', '#uelinton', function(){
+  alert("Resposta Errada! Tente Novamente");
+  var correto = "errado";
+});
+
+$(document).on('click', '#q1', function(){
+  window.location = "questao2.html";
+  
+});
+
+$(document).on('click', '#q2', function(){
+  window.location = "questao3.html";
+  
+});
+
+$(document).on('click', '#q3', function(){
+  window.location = "questao4.html";
+  
+});
+
+$(document).on('click', '#q4', function(){
+  window.location = "questao5.html";
+  
+});
+
+$(document).on('click', '#q5', function(){
+  window.location = "questao6.html";
+  
+});
+
+$(document).on('click', '#q6', function(){
+  window.location = "questao7.html";
+  
+});
+
+$(document).on('click', '#q7', function(){
+  window.location = "questao8.html";
+  
+});
+
+$(document).on('click', '#q8', function(){
+  window.location = "questao9.html";
+  
+});
+
+$(document).on('click', '#q9', function(){
+  window.location = "questao10.html";
+  
+});
+
+$(document).on('click', '#q10', function(){
+  window.location = "fim.html";
+  
+});
+
+$(document).on('click', '#q11', function(){
+  window.location = "index.html";
+  
+});
